60258b5a21383f2e60beb89f58a11a8fb3a1a826
Vitaliy <theyamshikov@yandex.ru>
2021-01-15 16:26:03 +0600

    upd

---
+++ src/Fork/yiisoft/yii/di/Container.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZnYii\App\Fork\yiisoft\yii\di;
+
+use Psr\Container\ContainerInterface;
+
+class Container extends \yii\di\Container implements ContainerInterface
+{
+
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,13 +2,7 @@
 
 namespace ZnYii\App;
 
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use ZnCore\Base\Enums\Measure\TimeEnum;
-use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
-use ZnCore\Base\Libs\App\Interfaces\LoaderInterface;
 use ZnCore\Base\Libs\DotEnv\DotEnv;
-use ZnYii\App\Loader\AdvancedLoader;
-use ZnYii\App\Loader\BaseLoader;
 
 class Kernel extends \ZnCore\Base\Libs\App\Kernel
 {
@@ -17,15 +11,12 @@ class Kernel extends \ZnCore\Base\Libs\App\Kernel
     {
         DotEnv::init(__DIR__ . '/../../../..');
     }
-    
+
     public function booststrap(array $env)
     {
-        /*if ($env === null) {
-            $env = $this->env;
-        }*/
         Constant::defineEnv($env);
-        include __DIR__ . '/../../../yiisoft/yii2/Yii.php';
+        include __DIR__ . '/../../../znyii/app/src/Fork/yiisoft/yii/di/Container.php';
+        include __DIR__ . '/../../../znyii/app/src/Fork/yiisoft/yii/Yii.php';
         Constant::defineBase(realpath(__DIR__ . '/../../../..'));
-        //Constant::defineApp($this->appName);
     }
 }
