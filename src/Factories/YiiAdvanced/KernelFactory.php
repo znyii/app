39e2feb198024360ff02fabdb7ec19fac075badc
Vitaliy <theyamshikov@yandex.ru>
2021-05-09 17:42:21 +0600

    upd

---
+++ src/Factories/YiiAdvanced/KernelFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ZnYii\App\Factories\YiiAdvanced;
+
+use ZnCore\Base\Libs\App\Interfaces\LoaderInterface;
+use ZnCore\Base\Libs\App\Kernel;
+use ZnCore\Base\Libs\App\Loaders\BundleLoader;
+use ZnYii\App\Loader\AdvancedLoader;
+
+class KernelFactory extends \ZnCore\Base\Libs\App\Factories\KernelFactory
+{
+
+    public static function createConsoleKernel(array $bundles = []): Kernel
+    {
+        return self::createKernel('console', ['i18next', 'container', 'console', 'migration']);
+    }
+
+    public static function createFrontendKernel(array $bundles = []): Kernel
+    {
+        return self::createKernel('frontend', ['i18next', 'container', 'yiiWeb']);
+    }
+
+    public static function createBackendKernel(): Kernel
+    {
+        return self::createKernel('backend', ['i18next', 'container', 'yiiAdmin']);
+    }
+
+    protected static function createKernel(string $appName, array $import): Kernel
+    {
+        self::init();
+        $bundleLoader = new BundleLoader([], $import);
+        self::loadBundles($bundleLoader, $appName);
+        $kernel = new \ZnYii\App\Kernel($appName);
+        $kernel->setLoader(new AdvancedLoader(__DIR__ . '/../../../../../../' . $appName));
+        $kernel->setLoader($bundleLoader);
+        return $kernel;
+    }
+
+    protected static function loadBundles(LoaderInterface $bundleLoader, string $appName)
+    {
+        $bundleLoader->addBundles(include __DIR__ . '/../../../../../../' . $appName . '/config/extra/bundle.php');
+        $bundleLoader->addBundles(include __DIR__ . '/../../../../../../common/config/extra/bundle.php');
+    }
+}
