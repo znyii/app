de9f9ca2971d71d39d4dc0ad3221482147610e38
Vitaliy <theyamshikov@yandex.ru>
2021-05-14 18:27:48 +0600

    upd

--- src/Fork/yiisoft/yii/di/Container.php
+++ src/Fork/yiisoft/yii/di/Container.php
@@ -3,8 +3,29 @@
 namespace ZnYii\App\Fork\yiisoft\yii\di;
 
 use Psr\Container\ContainerInterface;
+use ZnCore\Base\Helpers\ClassHelper;
+use ZnCore\Base\Libs\App\Helpers\ContainerHelper;
+use ZnCore\Domain\Helpers\EntityHelper;
 
 class Container extends \yii\di\Container implements ContainerInterface
 {
 
+    /*public function get($class, $params = [], $config = [])
+    {
+        $object = ContainerHelper::getContainer()->make($class, $params);
+        EntityHelper::setAttributes($object, $config);
+        return $object;
+    }
+
+    protected function build($class, $params, $config)
+    {
+        $object = ContainerHelper::getContainer()->make($class, $params);
+        EntityHelper::setAttributes($object, $config);
+        return $object;
+    }
+
+    public function has($class)
+    {
+        return ContainerHelper::getContainer()->has($class);
+    }*/
 }
